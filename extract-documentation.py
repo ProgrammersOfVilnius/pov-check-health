b849d77983fa8277638b4b7c2374667d27e97bb3
Marius Gedminas <marius@gedmin.as>
2013-07-23 15:44:21 +0300

    extract-documentation.py: don't lose the first comment

--- extract-documentation.py
+++ extract-documentation.py
@@ -9,7 +9,7 @@ doc_rx = re.compile('^\n(# [a-zA-Z_0-9]+.*\n(?:#.*\n)+)', re.MULTILINE)
 def extract_docs(filename='functions.sh'):
     with open(filename) as f:
         code = f.read()
-    checks = code.partition(checks_marker)[-1]
+    checks = '\n' + code.partition(checks_marker)[-1]
     docs = doc_rx.findall(checks)
     return docs
 
