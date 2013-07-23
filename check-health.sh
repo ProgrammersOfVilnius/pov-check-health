528b092a3ecb4a8d193c55fb74ade68bf193eb71
Marius Gedminas <marius@gedmin.as>
2013-07-23 11:55:13 +0300

    check-health -v and comments for every check

--- check-health.sh
+++ check-health.sh
@@ -7,6 +7,11 @@ test -f /etc/pov/check-health || exit 0
 
 . /usr/share/pov-check-health/functions.sh
 
+if [ x"$1" = x"-v" ]; then
+    verbose=1
+    shift
+fi
+
 . /etc/pov/check-health
 
 exit 0
--- functions.sh
+++ functions.sh
@@ -6,8 +6,16 @@
 # Helpers
 #
 
+verbose=0
+
 warn() {
     echo "$@" 1>&2
+}
+
+info() {
+    if [ $verbose -gt 0 ]; then
+        echo "$@"
+    fi
 }
 
 # convert value to seconds
@@ -61,30 +69,65 @@ _to_mb() {
 # Checks
 #
 
-# if uptime is too low, the system probably hasn't fully booted up yet,
-# so it doesn't make to check that all the services have started
+# checkuptime [<uptime>[s/m/h]]
+#   Skip the rest of the checks if system uptime is less than N
+#   seconds/minutes/hours
+#
+#   <uptime> defaults to 10 minutes
+#
+#   Example: checkuptime 10m
 checkuptime() {
+    info checkuptime $@
     want=$(_to_seconds $1 600)
     uptime=$(cut /proc/uptime -d . -f 1)
     if [ $uptime -lt $want ]; then
+        info "uptime less than $want seconds, skipping the rest of the checks"
         exit 0
     fi
 }
 
+# checkfs <mountpoint> [<amount>[K/M/G/T]]
+#   Check that the filesystem mounted on <mountpoint> has at least <amount>
+#   of metric kilo/mega/giga/terabytes free.
+#
+#   <amount> defaults to 1M
+#
+#   Example: checkfs / 100M
 checkfs() {
+    info checkfs $@
     need=$(_to_kb $2 1000)
     free=$(df -P -k $1 | awk 'NR==2 { print $4; }')
     [ -z "$free" ] && { warn "couldn't figure out free space in $1"; return; }
     [ $free -lt $need ] && warn "$1 is low on disk space ($free)"
 }
 
+# checkinodes <mountpoint> [<inodes>]
+#   Check that the filesystem mounted on <mountpoint> has at least <inodes>
+#   of free inodes left.
+#
+#   <inodes> defaults to 5000
+#
+#   Example: checkinodes /
 checkinodes() {
+    info checkinodes $@
     need=${2:-5000}
     free=$(df -i $1 | awk 'NR==2 { print $4; }')
     [ $free -lt $need ] && warn "$1 is low on inodes ($free)"
 }
 
+# checknfs <mountpoint>
+#   Check that an NFS file system is mounted on <mountpoint>.
+#
+#   If not, try to mount all NFS filesystems.
+#
+#   Used as a workaround for an Ubuntu issue where NFS filesystems would fail
+#   to mount during boot, but would mount fine afterwards.
+#
+#   This hasn't been a problem lately.
+#
+#   Example: checknfs /home
 checknfs() {
+    info checknfs $@
     [ -z "$(grep "$1 .* nfs " /proc/mounts)" ] && {
         warn "$1 is not NFS-mounted, trying to remount"
         mount -a -t nfs
@@ -92,20 +135,31 @@ checknfs() {
     }
 }
 
+# checkpidfile <filename>
+#   Check that the process listed in a given pidfile is running.
+#
+#   Example: checkpidfile /var/run/crond.pid
 checkpidfile() {
+    info checkpidfile $@
     [ -f $1 ] || { warn "$1: pidfile missing"; return; }
     pid="$(cat $1)"
     test -d "/proc/$pid" || warn "$1: stale pidfile ($pid)"
 }
 
+# checkpidfiles <filename> ...
+#   Check that the process listed in given pidfiles are running.
+#
+#   Example: checkpidfiles /var/run/*.pid /var/run/*/*.pid
 checkpidfiles() {
     for pidfile in "$@"; do
         case $pidfile in
             "/var/run/*/*.pid")
                 # suppress spurious warning when this glob doesn't match anything
+                info "ignoring $pidfile since glob failed to match"
                 ;;
             /var/run/sm-notify.pid)
                 # ignore: this one is always stale, yet nothing bad happens
+                info "ignoring $pidfile since it's always stale"
                 ;;
             *)
                 checkpidfile $pidfile
@@ -115,35 +169,89 @@ checkpidfiles() {
     done
 }
 
+# checkproc <name>
+#   Check that a process with a given name is running.
+#
+#   See also: checkproc_pgrep, checkproc_pgrep_full
+#
+#   Example: checkproc crond
 checkproc() {
+    info checkproc $@
     [ -z "$(pidof -s $1)" ] && warn "$1 is not running"
 }
 
+# checkproc_pgrep <name>
+#   Check that a process with a given name is running.
+#
+#   Uses pgrep instead of pidof, which makes it handle scripts too.
+
+#   (XXX why didn't I use pidof -x?  ignorance?)
+#
+#   Example: checkproc_pgrep tracd
 checkproc_pgrep() {
+    info checkproc_pgrep $@
     [ -z "$(pgrep $1)" ] && warn "$1 is not running"
 }
 
+# checkproc_pgrep_full <cmdline>
+#   Check that a process matching a given command line is running.
+#
+#   Uses pgrep -f instead of pidof, which makes it handle all sorts of things.
+#
+#   Example: checkproc_pgrep_full scriptname.py
+#   Example: checkproc_pgrep_full '/usr/bin/java -jar /usr/share/jenkins/jenkins.war'
 checkproc_pgrep_full() {
+    info checkproc_pgrep_full $@
     [ -z "$(pgrep -f "$@")" ] && warn "$1 is not running"
 }
 
+# checktoomanyproc <name> <limit>
+#   Check that fewer than <limit> instances of a given process is running.
+#
+#   Example: checktoomanyproc aspell 2
 checktoomanyproc() {
+    info checktoomanyproc $@
     [ "$(pidof $1|wc -w)" -ge "$2" ] && warn "More than $2 copies of $1 running"
 }
 
+# checkram [<free>[M/G/T]]
+#   Check that at least <free> metric mega/giga/terabytes of virtual memory are
+#   free.
+#
+#   <free> defaults to 100 megabytes
+#
+#   Example: checkram 100M
 checkram() {
+    info checkram $@
     need=$(_to_mb $1 100)
     free=$(free -mt | awk '$1 ~ /^Total/ { print $4; }')
     [ $free -lt $need ] && warn "low on virtual memory ($free)"
 }
 
+# checkswap [<limit>[M/G/T]]
+#   Check if more than <limit> metric mega/giga/terabytes of swap are used.
+#
+#   <limit> defaults to 100 megabytes
+#
+#   Example: checkswap 2G
 checkswap() {
+    info checkswap $@
     trip=$(_to_mb $1 100)
     used=$(free -m| awk '/^Swap/ {print $3}')
     [ $used -gt $trip ] && warn "too much swap used ($used)"
 }
 
+# checkmailq [<limit>]
+#   Check if more than <limit> emails are waiting in the outgoing mail queue.
+#
+#   <limit> defaults to 20
+#
+#   The check is silently skipped if you don't have any MTA (that provides a
+#   mailq command) installed.  Otherwise it probably works only with Postfix.
+#
+#   Example: checkmailq 100
 checkmailq() {
+    info checkmailq $@
     # this probably only works with postfix
     limit=${1:-20}
     status=$(mailq 2>&1| tail -n 1)
@@ -156,13 +264,33 @@ checkmailq() {
     [ $count -gt $limit ] && warn "mail queue is large ($count requests)"
 }
 
+# checkzopemailq <path> ...
+#   Check if any messages older than one minute are present in the outgoing
+#   maildir used by zope.sendmail.
+#
+#   <path> needs to refer to the 'new' subdirectory of the mail queue.
+#
+#   Example: checkzopemailq /apps/zopes/*/var/mailqueue/new
 checkzopemailq() {
-    for f in `find /apps/zopes/*/var/mailqueue/new -type f -mmin +1`; do
+    info checkzopemailq $@
+    for f in $(find "$@" -type f -mmin +1); do
         warn "stale zope mail message: $f"
     done
 }
 
+# checkcups <queuename>
+#   Check if the printer is ready.
+#
+#   Try to enable it if it became disabled.
+#
+#   Background: I had this issue with CUPS randomly disabling a particular mail
+#   queue after it couldn't talk to the printer for a while due to network
+#   issues or something.  Manually reenabling the printer got old fast.
+#   This hasn't been a problem lately.
+#
+#   Example: checkcups cheese
 checkcups() {
+    info checkcups $@
     queuename=$1
     lpq $queuename | grep -s -q "^$queuename is not ready$" && {
         warn "printer $1 is not ready, trying to enable"
@@ -170,7 +298,16 @@ checkcups() {
     }
 }
 
+# cmpfiles <pathname1> <pathname2>
+#   Check if the two files are identical.
+#
+#   Background: there were some init.d scripts that were writable by a non-root
+#   user.  I wanted to do manual inspection before replacing copies of them
+#   into /etc/init.d/.
+#
+#   Example: cmpfiles /etc/init.d/someservice /home/someservice/initscript
 cmpfiles() {
+    info cmpfiles $@
     file1="$1"
     file2="$2"
     cmp -s "$file1" "$file2" || {
@@ -178,12 +315,32 @@ cmpfiles() {
     }
 }
 
+# checkaliases
+#   Check if /etc/aliases.db is up to date
+#
+#   Probably works only with Postfix, and only if you use the default database
+#   format.
+#
+#   Background: when you edit /etc/aliases it's so easy to forget to run
+#   newaliases.
+#
+#   Example: checkaliases
 checkaliases() {
+    info checkaliases $@
     [ /etc/aliases.db -ot /etc/aliases ] && {
         warn "/etc/aliases.db out of date; run newaliases"
     }
 }
 
+# checklilo
+#   Check if LILO was run after a kernel update
+#
+#   Background: if you don't re-run LILO after you update your kernel, your
+#   machine will not boot.  We had to use LILO on one server because GRUB
+#   completely refused to boot from the Software RAID-1 root partition.
+#
+#   Example: checklilo
 checklilo() {
+    info checklilo $@
     [ /boot/map -ot /vmlinuz ] && warn "lilo not updated after kernel upgrade"
 }
